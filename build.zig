fba60a93d7dc9e8bf64345e94783ac537a80b7ba
cjkenyon <cjkenyon01@gmail.com>
2026-05-08 23:09:46 -0500

    initial project

---
+++ build.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const mod = b.addModule("btuber", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "btuber",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "btuber", .module = mod },
+            },
+        }),
+    });
+    b.installArtifact(exe);
+}
